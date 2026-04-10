577f2c96b40f0b7125e0c92bad1ad5559dc7aa76
Daniel <niziu108@gmail.com>
2026-04-10 09:18:13 +0200

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,43 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </div>
 
           <ConsentScripts />
+
+          {/* META PIXEL */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  function initPixel() {
+                    if (window.fbq) return;
+
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;
+                    n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}
+                    (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+                    fbq('init', '1374422448037164');
+                    fbq('track', 'PageView');
+                  }
+
+                  if (localStorage.getItem('cookie-consent-marketing') === 'true') {
+                    initPixel();
+                  }
+
+                  window.addEventListener('cookieConsentAccepted', function(e) {
+                    if (e.detail?.marketing) {
+                      initPixel();
+                    }
+                  });
+                })();
+              `,
+            }}
+          />
+
           <CookieConsent />
         </Providers>
       </body>
