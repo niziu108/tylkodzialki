ad2cf4288d31e258b22adb43aa7a8de1046fb4ad
Daniel <niziu108@gmail.com>
2026-04-15 12:24:08 +0200

    favicon

--- src/lib/crm/getCurrentUser.ts
+++ src/lib/crm/getCurrentUser.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { prisma } from "@/lib/prisma";
-import { authOptions } from "@/src/auth-options";
+import { authOptions } from "@/auth-options";
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
